eb3d082d89c11d8f859d9772b3c6f2e5e5c758c7
James Goytia <jmsgoytia@gmail.com>
2020-02-25 13:58:30 -0800

    updated comp

--- src/components/StbCpe.vue
+++ src/components/StbCpe.vue
@@ -1,8 +1,21 @@
 <template>
-<div class="top-wrap">
+<div>
+  <div class="top-wrap">
     <div id="wrapper" @mouseover="mouseOver" @mouseleave="mouseLeave"> <!--<p id="wrapper"></p> --> </div>
-      <div class="hero-title" ><h1>STB-CPE</h1></div> 
+      <div class="hero-title" ><h1>STB-CPE</h1></div>
   </div>
+   <div class="head"><h3>stb / cpe services division</h3></div>
+   <div class="container">
+     <p>
+      In just ten years, CTDI has totally revolutionized the STB / CPE service business and has become the global leader in both the cable and telecom service industries.  CTDI has in-depth experience with many in-region, high-volume STB / CPE test, repair and logistics programs. This allows us to provide our customers a comprehensive end-to-end, integrated solution that not only leverages CTDI’s extensive CPE management expertise, but also provides high-quality, web-based control and management for all test, repair and logistics activity via ETOS. Our ability to establish regional hubs provides our customers with better control of their assets, eliminates transportation and customs fees, and greatly enhances service levels and product availability.  Numerous OEMs have certified CTDI’s CPE operations and test systems to perform in-and-out-of-warranty test and repair services.
+      </p>
+     <p>
+       Today, CTDI’s STB operations network now includes 35 world-class facilities supported by over 9,500 CPE service professionals who are authorized to test and repair a variety of Carrier, MSO and OEM STBs, Modems, Gateways and Tablet Devices. CTDI offers a fully integrated service model whereby we provide in-region screen & clean, testing, refurbishment, warehousing and order fulfillment services, including Direct to Home. Today, CTDI’s innovative multi-unit test systems, the Gen-5 AND Gen-7 STB tester, CMTX 5.0 Modem and Gateway tester, help us test and process over 78 million units annually.
+     </p>
+     <img src="../assets/CTDI-Services/ctdi-stb.jpg" alt= "ctdi facility" id="stbImg">
+     <p>CTDI also has over 30 of our STB / CPE operations OEM certified to perform in-warranty repair for STBs, Gateways and Tablet Devices.  OEM certifications include: ARRIS, Technicolor, Samsung, and Amazon. </p>
+   </div>
+</div>
   </template>
 
   <script>
@@ -89,30 +102,17 @@
 
 
 </script>
+
 <style scoped>
 
-.statement {
-    max-width: 80%;
-    margin: auto;
-    padding-bottom: 35px;
-    margin-bottom: 75px;
-  }
-
-  .statement h5 {
-    font-size: 42px;
-    line-height: 49px;
-    color: #111111;
-    font-weight: 600;
-    text-align: center;
-    padding-bottom: 50px;
-    padding-top: 135px;
-  }
 
   .top-wrap{
      width: 100%;
-      height: 100vh;
-      background: #161616;
+     height: 100vh;
+     background: #161616;
   }
+
+
 
 #wrapper {
       margin-top: 50px;
@@ -160,18 +160,30 @@ h1 {
   font-family: "Cosi Azure" !important;
 }
 
+    .head h3{
+    font-size: 42px;
+    line-height: 49px;
+    font-weight: 600;
+    text-align: center;
+    color: #202124;
+    margin-bottom: 150px;
+    margin-top: 200px;
+    text-transform: uppercase;
+  }
 
+p{
+    font-size: 17px;
+    line-height: 26px;
+    color: #202124;
+    text-align: left; 
+}
 
-
-h6 {
-  max-width: 80%;
-  text-align: center;
-  font-weight: 600;
-  color: #202124;
-  font-size: 23px;
-  padding-top: 150px;
-  padding-bottom: 45px;
-  margin: auto;
+#stbImg{
+  width: 600px;
+  height: 400px;
+  border-radius: 4px;
+  margin-top: 50px;
+  margin-bottom: 50px;
 }
 
 
@@ -182,11 +194,7 @@ h6 {
   max-width: 75%;
 }
 
-h5 {
-  color: #111111;
-  font-weight: bold;
-  font-size: 18px;
-}
+
 
 a {
   text-decoration: none !important;
