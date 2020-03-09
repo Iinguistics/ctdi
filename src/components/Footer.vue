49e34502a0c3c13f8527c98cfd475108e25dba1e
Iinguistics <43329975+Iinguistics@users.noreply.github.com>
2020-03-09 05:00:24 -0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="flex-container container-fluid"> 
+  <div class="flex-container "> 
   <li class="item1"><h3>Contact Us</h3><h5>CTDI Global Headquaters</h5><p>Communications Test Design,Inc</p>
   <p>1373 Enterprise Drive
      West Chester, PA 19380</p>
