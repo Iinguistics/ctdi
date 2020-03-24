19b8731299bdf3de696f3e3800117bbe652bc274
Iinguistics <43329975+Iinguistics@users.noreply.github.com>
2020-03-24 14:41:08 -0700

    Update Community.vue

--- src/components/Community.vue
+++ src/components/Community.vue
@@ -26,6 +26,33 @@
        <div class="passion-item">
        <p>CTDI also sources employees from work rehabilitation programs such as <a href="https://handi-crafters.org/" target="_blank">Handi-Crafters</a> & <a href="https://www.bakerindustries.org/" target="_blank">Baker Industries</a> for staffing as needed.</p>
        </div>
+         <div class="divider"></div>
+      <!-- hover overlay -->
+   <div class="overlay-container">
+  <div class="content">
+    <router-link to=/sustainability/environment>
+      <div class="content-overlay"></div>
+      <img class="content-image" src="../assets/CTDI-Sustainability/nature.jpg" alt="field of flowers">
+      <div class="content-details fadeIn-left">
+        <h3>Environment</h3>
+        <p>CTDI has made a commitment that we will continue to train and educate ourselves in waste minimization, hazardous material handling and regulatory compliance in an effort to support the longevity of the environment.</p>
+      </div>
+       <h6>Environment</h6>
+    </router-link>
+  </div>
+
+  <div class="content">
+    <router-link to=/sustainability/ethics>
+      <div class="content-overlay"></div>
+      <img class="content-image" src="../assets/CTDI-Sustainability/ethics-1.jpg" alt="cabinet full of books"> 
+      <div class="content-details fadeIn-right">
+        <h3>Ethics</h3>
+        <p>CTDI and its subsidiaries conduct business consistent with our long established corporate values.</p>
+      </div>
+       <h6>Ethics</h6>
+    </router-link>
+  </div>
+</div>
         <img class="img-fluid passion-item" src="../assets/CTDI-Community/person-working.jpg" alt="person working">
      </div>
      <div id="to-top"><a class="fa fa-arrow-circle-o-up"  v-on:click="toTop">Back to top</a></div>
@@ -272,6 +299,100 @@
     margin-top: 60px;
 }
 
+
+/* hover overlay */
+.content {
+   position: relative;
+  width: 100%;
+  max-width: 375px;
+  margin: auto;
+  overflow: hidden; 
+}
+
+
+.content .content-overlay {
+  background: rgba(0,0,0,0.7);
+  position: absolute;
+  height: 225px;
+  width: 100%;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 0;
+  -webkit-transition: all 0.4s ease-in-out 0s;
+  -moz-transition: all 0.4s ease-in-out 0s;
+  transition: all 0.4s ease-in-out 0s;
+}
+
+.content:hover .content-overlay{
+  opacity: 1;
+}
+
+
+.content-image{
+  width: 405px;
+  height: 225px;
+}
+
+.content-details {
+  position: absolute;
+  text-align: center;
+  padding-left: 1em;
+  padding-right: 1em;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  opacity: 0;
+  -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  -webkit-transition: all 0.3s ease-in-out 0s;
+  -moz-transition: all 0.3s ease-in-out 0s;
+  transition: all 0.3s ease-in-out 0s;
+}
+
+.content:hover .content-details{
+  top: 50%;
+  left: 50%;
+  opacity: 1;
+}
+
+.content-details h3{
+  color: #fff;
+  font-weight: 500;
+  letter-spacing: 0.15em;
+  margin-bottom: 0.5em;
+  text-transform: uppercase;
+}
+
+.content-details p{
+  color: #fff;
+  font-size: 0.9em;
+}
+
+.fadeIn-top{
+  top: 20%;
+}
+
+.fadeIn-left{
+  left: 20%;
+}
+
+.fadeIn-right{
+  left: 80%;
+}
+
+.overlay-container{
+  max-width: 85%;
+  height: 600px;
+  display: flex;
+  flex-direction: row;
+  margin: auto;
+  justify-content: space-between;
+  padding-bottom: 40px;
+}
+
 #to-top{
  text-align: right;
  color: #202124;
@@ -334,6 +455,17 @@
  #statement{
    display: none;
  }
+
+ .overlay-container{
+  max-width: 85%;
+  height: 650px;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  justify-content: space-evenly;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
 
 }
 
